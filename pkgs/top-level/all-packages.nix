37a44c9631cc3adce031751c08f6f4e588215eaa
Thomas Tuegel <ttuegel@mailbox.org>
2020-09-25 05:34:24 -0500

    vlc: do not build with every Qt version
    It is no longer necessary to build vlc with every Qt version now that we have a
    Qt-less libvlc.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15147,8 +15147,6 @@ in
 
     telepathy = callPackage ../development/libraries/telepathy/qt { };
 
-    vlc = callPackage ../applications/video/vlc {};
-
     qtwebkit-plugins = callPackage ../development/libraries/qtwebkit-plugins { };
 
   };
@@ -23932,7 +23930,7 @@ in
 
   vkeybd = callPackage ../applications/audio/vkeybd {};
 
-  vlc = libsForQt514.vlc;
+  vlc = libsForQt514.callPackage ../applications/video/vlc {};
 
   vlc_qt5 = vlc;
 
